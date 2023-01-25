74df9fb97c28bc71b4d71893320fa743c8cba8b8
poow810 <poow810@naver.com>
2023-01-26 00:21:36 +0900

    Ch2_ex

---
+++ Ch2_ex.py
@@ -0,0 +1,15 @@
+# 2.1 정수 99를 변수 prince에 할당
+prince = 99
+print(prince)
+
+# 2.2 값 5는 어떤 type?
+a = 5
+print(type(a))
+
+# 2.3 값 2.0은 어떤 type?
+b = 2.0
+print(type(b))
+
+# 2.4 표현식 5 + 2.0은 어떤 type?
+c = 5 + 2.0
+print(type(c))
