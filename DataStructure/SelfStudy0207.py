b0361fdb9286bc448082b97634be318d4af9073f
poow810 <poow810@naver.com>
2023-02-07 18:08:51 +0900

    selfstudy0207_4

--- DataStructure/SelfStudy0207.py
+++ DataStructure/SelfStudy0207.py
@@ -1,74 +1,54 @@
-# 8-1
+## 함수 선언부
 class TreeNode():
     def __init__(self):
-        self.right = None
         self.left = None
         self.data = None
+        self.right = None
 
 
-def preorder(node):
-    if node == None:
-        return
-    print(node.data, end='->')
-    preorder(node.left)
-    preorder(node.right)
+def search(group):
+    global current, root, left, right
+    current = root
+    while True:
+        if group == current.data:
+            print(f"{group}을(를) 찾음")
+            break
+        elif group < current.data:
+            if current.left == None:
+                print(f"{group}이(가) Tree에 없음")
+                break
+            current = current.left
+        else:
+            if current.right == None:
+                print(f"{group}이(가) Tree에 없음")
+                break
+            current = current.right
+
+root = None
+nameAry = ['블랙핑크', '레드벨벳', '마마무', '에이핑크', '걸스데이', '트와이스', '잇지', '여자친구']
+node = TreeNode()
+node.data = nameAry[0]
+root = node
+
+for name in nameAry[1:]:
+
+    node = TreeNode()
+    node.data = name
+
+    current = root
+    while True:
+        if name < current.data:
+            if current.left == None:
+                current.left = node
+                break
+            current = current.left
+        else:
+            if current.right == None:
+                current.right = node
+                break
+            current = current.right
 
 
-def inorder(node):
-    if node == None:
-        return
-    inorder(node.left)
-    print(node.data, end='->')
-    inorder(node.right)
-
-
-def postorder(node):
-    if node == None:
-        return
-    postorder(node.left)
-    postorder(node.right)
-    print(node.data, end='->')
-
-
-node1 = TreeNode()
-node1.data = "화사"
-
-node2 = TreeNode()
-node2.data = "솔라"
-node1.left = node2
-
-node3 = TreeNode()
-node3.data = "문별"
-node1.right = node3
-
-node4 = TreeNode()
-node4.data = "휘인"
-node2.left = node4
-
-node5 = TreeNode()
-node5.data = "쯔위"
-node2.right = node5
-
-node6 = TreeNode()
-node6.data = "선미"
-node3.left = node6
-
-node7 = TreeNode()
-node7.data = "다현"
-node4.right = node7
-
-node8 = TreeNode()
-node8.data = "사나"
-node6.right = node8
-
-print('전위 순회 : ', end=' ')
-preorder(node1)
-print('끝')
-
-print('중위 순회 : ', end=' ')
-inorder(node1)
-print('끝')
-
-print('후위 순회 : ', end=' ')
-postorder(node1)
-print('끝')
+if __name__=="__main__":
+    group = input("찾을 그룹이름 --> ")
+    search(group)
