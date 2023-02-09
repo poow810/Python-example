4cfc9343d3d0bbc042086750f0c6653c5d5d2556
poow810 <poow810@naver.com>
2023-02-09 16:35:11 +0900

    selfstudy 10-2

--- DataStructure/SelfStudy0209.py
+++ DataStructure/SelfStudy0209.py
@@ -1,12 +1,10 @@
-# 10-1 두 수를 입력 받고 두 숫자 사이의 합계를 구하는 코드
-def add_number(num1, num2):
-    if num1 > num2:
-        num1, num2 = num2, num1
-    if num2 <= num1:
-        return num1
-    return num2 + add_number(num1, num2 - 1)
+# 10-2 구구단을 세로로 작성하기
+def gugu_dan(dan, num):
+    print(f"{dan} * {num} = {dan*num}")
+    if dan < 10 :
+        if num < 9:
+            return gugu_dan(dan, num+1)
 
 
-num1 = int(input())
-num2 = int(input())
-print(add_number(num1, num2))
+for i in range(2, 10):
+    gugu_dan(i, 1)
