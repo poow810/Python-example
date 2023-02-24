fa436f54ebf03ad5c7b8a6c9eddaa9e4f4554407
poow810 <poow810@naver.com>
2023-02-24 20:36:58 +0900

    stack -2

--- DataStructure/Stack.py
+++ DataStructure/Stack.py
@@ -1,57 +1,54 @@
-# 스택에 데이터 삽입
+# 헨젤과 그레텔의 집으로 돌아가기
+import random
+
+size = 10
+stack = [None for _ in range(size)]
+top = -1
+
+def is_stack_full():
+    global size, stack, top
+    if top >= size-1:
+        return True
+    else:
+        return False
+
+
+def is_stack_empty():
+    global size, stack, top
+    if top == -1:
+        return True
+    else:
+        return False
+
+
 def push(data):
-    global Size, top, stack
-    top += 1
+    global size, stack, top
+    if is_stack_full():
+        return
+    top = top + 1
     stack[top] = data
 
 
-# 스택에서 데이터 추출
 def pop():
-    global Size, top, stack
+    global size, stack, top
+    if is_stack_empty():
+        return None
     data = stack[top]
     stack[top] = None
-    top -= 1
+    top = top - 1
     return data
 
 
-def is_empty_stack():
-    global stack, Size, top
-    if (top == -1):
-        return True
-    else:
-        return False
-
-# 응용
-def check_bracket(word):
-    for i in word:
-        if i in '([{<':
-            push(i)
-        elif i in ')]}>':
-            out = pop()
-            if i == ')' and out == '(':
-                pass
-            elif i == ']' and out == '[':
-                pass
-            elif i == '}' and out == '{':
-                pass
-            elif i == '>' and out == '<':
-                pass
-            else:
-                return False
-        else:
-            pass
-    if is_empty_stack():
-        return True
-    else:
-        return False
-
-Size = 100
-stack = [None for _ in range(Size)]
-top = -1
-
 if __name__=="__main__":
-
-    words = ['(A+B)', ')A+B(', '((A+B)-C', '(A+B]', '(<A+{B-C}/[C*D]>)']
-    for i in words:
-        top = -1
-        print(i, ' : ', check_bracket(i))
+    color = ["빨강", "주황", "노랑", "초록", "파랑", "보라"]
+    random.shuffle(color)
+    for i in color:
+        push(i)
+        print(i, '-->', end=" ")
+    print("과자집")
+    while True:
+        i = pop()
+        if i == None:
+            break
+        print(i, '-->', end=" ")
+    print("우리집")
